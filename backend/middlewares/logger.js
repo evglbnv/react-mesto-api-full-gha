38dbbb16fce59321a151cd30665b908bade9d91f
Evgeniy Lobanov <evglbnv@yandex.ru>
2023-06-03 21:00:24 +0300

    logging fix

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -1,14 +1,13 @@
-const expressWinston = require('express-winston');
 const winston = require('winston');
+const expressWinston = require('express-winston');
 
 // логгер запросов
 const requestLogger = expressWinston.logger({
-  transports: [
+  transports: [ // transports отвечает за то, куда нужно писать лог
     new winston.transports.File({ filename: 'request.log' }),
   ],
-  format: winston.format.json(),
+  format: winston.format.json(), // format отвечает за формат записи логов
 });
-
 // логгер ошибок
 const errorLogger = expressWinston.errorLogger({
   transports: [
